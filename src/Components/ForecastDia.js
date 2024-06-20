ef3d21edfd3f7ebe044b55fa3689a25c74112c22
Federico Verón <fedeva96@gmail.com>
2024-06-20 12:06:26 -0400

    detalle arreglado en width de forecast

--- src/Components/ForecastDia.js
+++ src/Components/ForecastDia.js
@@ -25,7 +25,7 @@ const ForecastDia = ({ weatherData }) => {
   //console.log(forecast);
 
   return (
-    <aside className="flex-1 flex flex-row items-center justify-center overflow-x-auto w-[90%] m-auto rounded-md no-scrollbar">
+    <aside className="flex-1 flex items-center justify-center overflow-x-auto w-full m-auto no-scrollbar">
       <div className="mt-4 flex flex-auto gap-2 w-full">
         {forecast.map((day) => (
           <div
