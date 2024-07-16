a0f7ad8d615ba4790a0ab70ed591536d0109d967
Federico Verón <fedeva96@gmail.com>
2024-07-16 11:37:48 -0400

    preferences actions creada y funcional

---
+++ src/Actions/PreferencesActions.js
@@ -0,0 +1,98 @@
+export const getSaveCities = async (userEmail) => {
+  const uri = `http://localhost:3001/api/users/${userEmail}/cities`;
+  const getOptions = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("jwt"),
+    },
+  };
+  const response = await fetch(uri, getOptions);
+  const data = response.json();
+  //console.log(data);
+  return data;
+};
+
+export const saveCity = async (userEmail, newCity) => {
+  const uri = `http://localhost:3001/api/users/${userEmail}/cities`;
+
+  // Obtén las ciudades actuales del usuario
+  const getOptions = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("jwt"),
+    },
+  };
+  const response = await fetch(uri, getOptions);
+  const currentCities = await response.json();
+
+  // Verifica si el número de ciudades actuales más la nueva excede el límite
+  if (currentCities.length >= 2) {
+    return "You can only save up to 2 cities";
+  }
+
+  // Si no se excede el límite, guarda la nueva ciudad
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("jwt"),
+    },
+    body: JSON.stringify(newCity),
+  };
+
+  await fetch(uri, postOptions);
+};
+
+export const removeCity = async (userEmail, cityName) => {
+  const uri = `http://localhost:3001/api/users/${userEmail}/cities/${cityName}`;
+  const getOptions = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("jwt"),
+    },
+  };
+  const response = await fetch(uri, getOptions);
+  if (response.status === 200) {
+    return "City removed";
+  } else {
+    return "City not removed";
+  }
+};
+
+export async function getPreferences(userEmail) {
+  const uri = `http://localhost:3001/api/users/${userEmail}/preferences`;
+  const getOptions = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("jwt"),
+    },
+  };
+  const response = await fetch(uri, getOptions);
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    return "No preferences found";
+  }
+}
+
+export async function setPreferences(userEmail, preferences) {
+  console.log(preferences);
+  const uri = `http://localhost:3001/api/users/${userEmail}/preferences`;
+  const postOptions = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("jwt"),
+    },
+    body: JSON.stringify(preferences),
+  };
+  const response = await fetch(uri, postOptions);
+  if (response.status === 200) {
+    return "Preferences updated";
+  } else {
+    return "Cant set preferences";
+  }
+}
